ae656a967b53b9708e0636abd13dd60ab78a16aa
Frida Johansson <frida_mikaela@hotmail.com>
2020-05-26 14:35:20 +0200

    camera

--- js/index.js
+++ js/index.js
@@ -1,3 +1,60 @@
+let stream = {};
+
+/*async function getLocation() {
+  try {
+    let coords = await getCurrentPosition();
+    console.log(coords);
+  } catch (error) {
+    console.log(error);
+  }
+}*/
+
+//funktion för att kunna ta en bild
+async function captureImage(stream) {
+  const mediaTrack = stream.getVideoTracks()[0];
+  console.log(mediaTrack);
+  const captureImg = new ImageCapture(mediaTrack);
+  const photo = await captureImg.takePhoto();
+  console.log(photo);
+  const imgUrl = URL.createObjectURL(photo); //URL är ett globalt objekt
+  console.log(imgUrl);
+  //document.querySelector('#photo').src = imgUrl
+}
+
+//funktion som frågar om sidan får åtkomst till min kamera
+async function getMedia() {
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: { min: 1280, min: 720 },
+    });
+    const videoElem = document.querySelector("#me");
+    videoElem.srcObject = stream; //en property man använder istället för src eftersom det är ett object vi får tillbaks och vi kan inte skriva ut ett objekt direkt i index.html
+    videoElem.addEventListener("loadedmetadata", () => {
+      videoElem.play();
+      setTimeout(() => {
+        captureImage(stream);
+      }, 1000);
+    });
+    console.log(stream);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMedia();
+
+/*document.querySelector("#addImage").addEventListener("click", (event) => {
+  captureImage(stream);
+});*/
+
+/*Caman("#photo", function () {
+  this.brightness(10);
+  this.contrast(30);
+  this.sepia(60);
+  this.saturation(-30);
+  this.render();
+});*/
+
 function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
@@ -8,58 +65,3 @@ function registerServiceWorker() {
 }
 
 registerServiceWorker();
-
-function getCurrentPosition() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
-/*async function getLocation() {
-  try {
-    let coords = await getCurrentPosition();
-    console.log(coords);
-  } catch (error) {
-    console.log(error);
-  }
-}*/
-
-//funktion som frågar om sidan får åtkomst till min kamera
-
-/*function captureImage(stream) {
-  const mediaTrack = stream.getVideoTracks()[0];
-  let imageCapture = new imageCapture(mediaTrack);
-  imageCapture.takePhoto().then(photo)=> {
-    console.log(photo)
-    let a = document.querySelector('#takePhoto');
-    a.href = URL.createObjectURL(photo)
-  }
-}*/
-
-/*async function captureImage(stream) {
-  const mediaTrack = stream.getVideoTracks()[0];
-  console.log(mediaTrack);
-  const captureImg = new imageCapture(mediaTrack);
-  const photo = await captureImg.takePhoto(mediaTrack);
-  console.log(photo);
-  const imgUrl = URL.createObjectURL(photo);
-  console.log(imgUrl);
-  document.querySelector("#photo").src = imgUrl;
-}*/
-
-async function getMedia() {
-  try {
-    stream = await navigator.mediaDevices.getUserMedia({
-      video: { min: 1280, min: 720 },
-    });
-    const videoElem = document.querySelector("#me");
-    videoElem.srcObject = stream; //en property man använder istället för src eftersom det är ett object vi får tillbaks och vi kan inte skriva ut ett objekt direkt i index.html
-    videoElem.addEventListener("loadedmetadata", () => {
-      videoElem.play();
-    });
-    console.log(stream);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-getMedia();
