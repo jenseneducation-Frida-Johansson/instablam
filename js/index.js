e9f65903e3a5225a9147d66ded12869a4bf15234
Frida Johansson <frida_mikaela@hotmail.com>
2020-05-27 11:37:34 +0200

    camera+take photo done

--- js/index.js
+++ js/index.js
@@ -1,13 +1,118 @@
-let stream = {};
+(function () {
+  var width = 320; // We will scale the photo width to this
+  var height = 0;
 
-/*async function getLocation() {
+  // |streaming| indicates whether or not we're currently streaming
+  // video from the camera. Obviously, we start at false.
+
+  var streaming = false;
+
+  // The various HTML elements we need to configure or control. These
+  // will be set by the startup() function.
+
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+
+  function startup() {
+    video = document.getElementById("video");
+    canvas = document.getElementById("canvas");
+    photo = document.getElementById("photo");
+    startbutton = document.getElementById("startbutton");
+
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(function (err) {
+        console.log("An error occurred: " + err);
+      });
+
+    video.addEventListener(
+      "canplay",
+      function (ev) {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+
+          // Firefox currently has a bug where the height can't be read from
+          // the video, so we will make assumptions if this happens.
+
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          canvas.setAttribute("width", width);
+          canvas.setAttribute("height", height);
+          streaming = true;
+        }
+      },
+      false
+    );
+
+    startbutton.addEventListener(
+      "click",
+      function (ev) {
+        takepicture();
+        ev.preventDefault();
+      },
+      false
+    );
+
+    clearphoto();
+  }
+
+  // Fill the photo with an indication that none has been
+  // captured.
+
+  function clearphoto() {
+    var context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+  }
+
+  // Capture a photo by fetching the current contents of the video
+  // and drawing it into a canvas, then converting that to a PNG
+  // format data URL. By drawing it on an offscreen canvas and then
+  // drawing that to the screen, we can change its size and/or apply
+  // other changes before drawing it.
+
+  function takepicture() {
+    var context = canvas.getContext("2d");
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      var data = canvas.toDataURL("image/png");
+      photo.setAttribute("src", data);
+    } else {
+      clearphoto();
+    }
+  }
+
+  // Set up our event listener to run the startup process
+  // once loading is complete.
+  window.addEventListener("load", startup, false);
+})();
+
+/*let stream = {};
+
+async function getLocation() {
   try {
     let coords = await getCurrentPosition();
     console.log(coords);
   } catch (error) {
     console.log(error);
   }
-}*/
+}
 
 //funktion för att kunna ta en bild
 async function captureImage(stream) {
@@ -36,23 +141,15 @@ async function getMedia() {
       }, 1000);
     });
     console.log(stream);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log("An error occured" + err);
   }
 }
 
 getMedia();
 
-/*document.querySelector("#addImage").addEventListener("click", (event) => {
+document.querySelector("#addImage").addEventListener("click", (event) => {
   captureImage(stream);
-});*/
-
-/*Caman("#photo", function () {
-  this.brightness(10);
-  this.contrast(30);
-  this.sepia(60);
-  this.saturation(-30);
-  this.render();
 });*/
 
 function registerServiceWorker() {
