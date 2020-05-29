7402e189193fbdebd601ee4229205f9c6765bdb7
Frida Johansson <frida_mikaela@hotmail.com>
2020-05-29 17:41:18 +0200

    Filters done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 (function () {
-  var width = 320; // We will scale the photo width to this
-  var height = 0; // This will be computed based on the input stream
+  var width = 320;
+  var height = 0;
 
   var streaming = false;
 
@@ -13,7 +13,7 @@
     video = document.getElementById("video");
     canvas = document.getElementById("canvas");
     photo = document.getElementById("photo");
-    startbutton = document.getElementById("startbutton");
+    startbutton = document.getElementById("camerabutton");
 
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
@@ -31,13 +31,6 @@
         if (!streaming) {
           height = video.videoHeight / (video.videoWidth / width);
 
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
-
-          if (isNaN(height)) {
-            height = width / (4 / 3);
-          }
-
           video.setAttribute("width", width);
           video.setAttribute("height", height);
           canvas.setAttribute("width", width);
@@ -50,7 +43,7 @@
 
     startbutton.addEventListener(
       "click",
-      function (ev) {
+      function () {
         takepicture();
         ev.preventDefault();
       },
@@ -60,9 +53,6 @@
     clearphoto();
   }
 
-  // Fill the photo with an indication that none has been
-  // captured.
-
   function clearphoto() {
     var context = canvas.getContext("2d");
     context.fillStyle = "#AAA";
@@ -71,12 +61,6 @@
     var data = canvas.toDataURL("image/png");
     canvas.setAttribute("data-caman-hidpi-disabled", data);
   }
-
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
 
   function takepicture() {
     var context = canvas.getContext("2d");
@@ -92,38 +76,90 @@
     }
   }
 
-  // Set up our event listener to run the startup process
-  // once loading is complete.
   window.addEventListener("load", startup, false);
 })();
 
-document.getElementById("brightness").addEventListener("change", (e) => {
+//Filter sliders
+let brightnessSlide = document.getElementById("brightness");
+let clipSlide = document.getElementById("clip");
+let exposureSlide = document.getElementById("exposure");
+let noiseSlide = document.getElementById("noise");
+let saturationSlide = document.getElementById("saturation");
+let sepiaSlide = document.getElementById("sepia");
+
+let brightness = 1;
+let clip = 1;
+let exposure = 1;
+let noise = 1;
+let saturation = 1;
+let sepia = 1;
+
+const changeFilter = () => {
   Caman("#canvas", function () {
-    /*this.brightness(e.target.value).render();*/
-    if (value == 0) {
-      this.revert();
-    }
-    console.log(value - oldValue);
-    this.brightness(value - oldValue);
-    oldValue = value;
+    this.revert(false);
+    this.brightness(brightness);
+    this.clip(clip);
+    this.exposure(exposure);
+    this.noise(noise);
+    this.saturation(saturation);
+    this.sepia(sepia);
     this.render();
   });
-});
+};
 
-var oldValue = 0;
-var value = 0;
+brightnessSlide.addEventListener(
+  "change",
+  () => {
+    brightness = brightnessSlide.value;
+    changeFilter();
+  },
+  true
+);
 
-document.getElementById("contrast").addEventListener("change", (e) => {
-  Caman("#canvas", function () {
-    this.contrast(e.target.value).render();
-  });
-});
+clipSlide.addEventListener(
+  "change",
+  () => {
+    clip = clipSlide.value;
+    changeFilter();
+  },
+  true
+);
 
-document.getElementById("exposure").addEventListener("change", (e) => {
-  Caman("#canvas", function () {
-    this.exposure(e.target.value).render();
-  });
-});
+saturationSlide.addEventListener(
+  "change",
+  () => {
+    saturation = saturationSlide.value;
+    changeFilter();
+  },
+  true
+);
+
+exposureSlide.addEventListener(
+  "change",
+  () => {
+    exposure = exposureSlide.value;
+    changeFilter();
+  },
+  true
+);
+
+noiseSlide.addEventListener(
+  "change",
+  () => {
+    noise = noiseSlide.value;
+    changeFilter();
+  },
+  true
+);
+
+sepiaSlide.addEventListener(
+  "change",
+  () => {
+    sepia = sepiaSlide.value;
+    changeFilter();
+  },
+  true
+);
 
 function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
